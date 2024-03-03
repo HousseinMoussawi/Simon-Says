e9425f66c71ea82a0c7d2fedd9026001422c053b
H-M-1999 <81930506@students.liu.edu.lb>
2024-03-04 00:51:25 +0200

    finished game

--- simon-says-game/js/main.js
+++ simon-says-game/js/main.js
@@ -8,136 +8,130 @@ const yellow=document.querySelector('.yellow');
 const tile = document.querySelectorAll('.tile');
 const board = document.querySelector('.board');
 
+red.id=0;
+blue.id=1;
+green.id=2;
+yellow.id=3;
+
+
 function blueSound(){
     let blue = new Audio('sounds/blue.mp3');
     blue.play();
 }
 
 function redSound(){    
-    let red = new Audio('../sounds/red.mp3');
+    let red = new Audio('sounds/red.mp3');
     red.play();
 }
 
 function greenSound(){        
-    let green = new Audio('../sounds/green.mp3');
+    let green = new Audio('sounds/green.mp3');
     green.play();
 }   
 
 function yellowSound(){            
-    let yellow = new Audio('../sounds/yellow.mp3');
+    let yellow = new Audio('sounds/yellow.mp3');
     yellow.play();
 }   
 
 function gameOverSound(){                
-    let gameOver = new Audio('../sounds/game-over.mp3');
+    let gameOver = new Audio('sounds/game-over.wav');
     gameOver.play();
 }   
 
 function wrongSound(){                    
-    let wrong = new Audio('../sounds/wrong.mp3');
+    let wrong = new Audio('sounds/wrong.mp3');
     wrong.play();
 }
 
 function gameWinSound(){                        
-    let gameWin = new Audio('../sounds/game-win.mp3');
+    let gameWin = new Audio('sounds/game-win.wav');
     gameWin.play();
 }
 
 const colors = ['red', 'blue', 'green', 'yellow'];
 
-let pattern=[];
+let pattern=0;
 let score=0;
 let level=1;
 
 
 function patternGenerator(){
-    if (level===1)
-    {
-        pattern.push(Math.floor(Math.random()*colors.length))
-    }
-    else if (level===2)
-    {
-        for (let i=0; i<2; i++){
-        pattern.push(Math.floor(Math.random()*colors.length))
-    }
-    }
-    else if (level===3)
-    {
-        for (let i=0; i<3; i++){
-        pattern.push(Math.floor(Math.random()*colors.length))
-    }
-    }    
-    else{
-        for (let i=0; i<4; i++){
-        pattern.push(Math.floor(Math.random()*colors.length))
-    }
-    }
-    console.log(pattern);
-    }
+        pattern = Math.floor(Math.random()*colors.length);
     
-
-function gameBegin(){
-    board.classList.remove('unclickable');
-    patternGenerator();
-    for (let i=0; i<pattern.length; i++){
-        if (pattern[i]==0){
-            red.classList.remove('inactive');
-            redSound();
-            red.classList.add('inactive');
-        }
-        else if (pattern[i]==1){
-            blue.classList.remove('inactive');
-            blueSound();
-            blue.classList.add('inactive');
-        }
-        else if (pattern[i]==2){
-            green.classList.remove('inactive');
-            greenSound();
-            green.classList.add('inactive');
-        }
-        else if (pattern[i]==3){
-            yellow.classList.remove('inactive');
-            yellowSound();
-            yellow.classList.add('inactive');
-        }
-    }
 }
 
+function gameBegin(){
+    pattern=0;
+    board.classList.remove('unclickable');
+    patternGenerator();
+        if (pattern==0){
+            red.classList.remove('inactive');
+            redSound();
+            setTimeout(function(){
+            red.classList.add('inactive');},1000)
+        }
+        else if (pattern==1){
+            blue.classList.remove('inactive');
+            blueSound();
+            setTimeout(function(){
+            blue.classList.add('inactive');},1000)
+        }
+        else if (pattern==2){
+            green.classList.remove('inactive');
+            greenSound();
+            setTimeout(function(){
+            green.classList.add('inactive');},1000)
+        }
+        else if (pattern==3){
+            yellow.classList.remove('inactive');
+            yellowSound();
+            setTimeout(function(){
+            yellow.classList.add('inactive');},1000)
+        }
+    }
+
+
 function gamePlay(){
-    gameBegin();  
-    for (let i=0; i<tile.length; i++){
-        tile[i].addEventListener('click', function(){
-            if (tile[i].innerHTML==pattern[i]){
+    pattern=0; 
+    gameBegin(); 
+    tile.forEach(function(tile){
+        tile.addEventListener('click', function(){
+            if (tile.id == pattern){
                 score++;
-                if (score==12){
+                highScore.innerHTML=score;
+                setTimeout(() => {
+                gameBegin();    
+                },300);
+                
+                if (score==12)
+                {
+                    score=0;
                     level++;
                     levels.innerHTML=level;
                     gameWinSound();
-                    score=0;
-                    pattern=[];
-                    gameBegin();
+                    highScore.innerHTML=score;
                 }
-            }
+                }
             else{
-                gameOverSound();
                 wrongSound();
+                setTimeout(() => {
+                gameOverSound();
+                },500);
                 score=0;
                 level=1;
-                pattern=[];
+                pattern=0;
                 levels.innerHTML=level;
-                highScore.innerHTML=score;
+                setTimeout(() => {
+                    window.location.reload();
+                }, 1000);
             }
         })
-    }  
+    })  
 
 }
 
 playbtn.addEventListener("click", function(){   
+    playbtn.classList.add('unclickable');
     gamePlay();
-    highScore.innerHTML=score;
-
-    if (score>highScore.innerHTML){
-        highScore.innerHTML=score;
-    }
-
 });
